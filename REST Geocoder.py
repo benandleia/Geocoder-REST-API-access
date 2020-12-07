29fa8363778112531a7a7508fb6af38a1ee2e56d
benandleia <75543646+benandleia@users.noreply.github.com>
2020-12-07 08:45:33 -0800

    REST Geocoder_no API key

---
+++ REST Geocoder.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  3 19:05:40 2020
+
+@author: SnowBe
+"""
+
+import requests
+import pandas as pd
+
+##################################################IMPORT THE WELL DATA #############################################################
+
+start_data = pd.read_csv('/Users/SnowBe/Desktop/well.csv')
+
+geographic = start_data[["longitude", "latitude"]]
+ 
+
+
+data_entries = len(geographic.index)
+
+full_address = []
+civic_number = []
+street = []
+locality = []
+long = []
+lat = []
+
+for i in range(10):
+    resp = requests.get('https://geocoder.api.gov.bc.ca/sites/nearest.json?point=' + str(geographic["longitude"][i]) + '%2C' +
+                        str(geographic['latitude'][i]))
+    if resp.status_code != 200:
+         # This means something went wrong.
+         raise Exception('Error Code {}'.format(resp.status_code))
+    data = resp.json()
+    full_address.append(data["properties"]["fullAddress"])
+    civic_number.append(data["properties"]["civicNumber"])
+    street.append(data["properties"]["streetName"])
+    locality.append(data["properties"]["localityName"])
+    long.append(geographic["longitude"][i])
+    lat.append( geographic['latitude'][i])
+    print("Processing Record {} of {}".format(i, data_entries))
+    
+#Assemble the dictionary, convert to a data frame
+
+output = {"locality":locality, "full_address": full_address, "civic_number": civic_number, "street_name": street, "latitude": lat, "longitude": long}
+
+output_df = pd.DataFrame(output)    
+
